4147873e63f34d4cc06279a6d21686ef40633c16
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-09-06 15:00:29 +0700

    generate user, update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     MongooseModule.forRootAsync({
@@ -12,6 +13,8 @@ import { MongooseModule } from '@nestjs/mongoose';
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
